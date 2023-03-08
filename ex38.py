641e4f728a9a8d2c1a862a5ad3f96ac1320bd9db
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-08 16:48:24 -0300

    done ex39

--- ex38.py
+++
@@ -1,8 +0,0 @@
-"""
-Leia o salário de um funcionário. Calcule e imprima o valor do novo salário, sabendo que ele recebeu um aumento de 25%
-"""
-
-sal = float(input("Informe o salário do funcionário: "))
-new_sal = sal*1.25
-
-print('O novo salário do funcionário é R$ {:.2f}'.format(new_sal))
