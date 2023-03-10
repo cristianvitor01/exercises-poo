1419d57cefe63304a9acc595300cdd72bf043786
CristianVitor1 <>
2023-03-10 12:41:16 +0000

    done ex01

---
+++ Lista02/ex01.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que receba um número inteiro e verifique se este número é par ou impar.
+"""
+num = int(input('Digite um número: '))
+
+if num % 2 == 0:
+  print('O número é par!')
+else:
+  print('O número ímpar!')
