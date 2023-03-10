ee8b1827d4337a47bf5488c6703120f6f30a8c91
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-10 16:46:10 -0300

    done ex03

--- Lista03/ex01.py
+++ Lista03/ex01.py
@@ -1,3 +1,8 @@
 """
 Faça um programa que leia um número inteiro N e depois imprima os N primeiros números naturais ímpares.
 """
+
+num = int(input('Informe um número inteiro: '))
+
+for i in range(1, 2*num, 2):
+    print(i)
