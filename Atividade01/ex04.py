1b7a04def9735d6af9c1347018e72438b3af0b69
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-17 17:13:39 -0300

    update ex04

--- Atividade01/ex04.py
+++ Atividade01/ex04.py
@@ -2,10 +2,18 @@
 4-Faça um Programa que leia lista  de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 """
 
-lista = ['v', 'a', 's', 'c', 'o']
+lista = []
 
 vogais = ['a', 'e', 'i', 'o', 'u']
+
+for i in range(5):
+    lista.append(input('Informe um caractere: '))
 
 cons = [x for x in lista if x not in vogais]
 
 print(cons)
+
+qtt = len(cons)
+
+print(f'Quantidade de consoantes: {qtt}')
+
