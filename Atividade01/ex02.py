ec23ea0f2695e43feca5556274f4f107a7ab5b3c
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-17 16:42:24 -0300

    done ex02

--- Atividade01/ex02.py
+++ Atividade01/ex02.py
@@ -2,3 +2,12 @@
 2-Faça um Programa que receba uma lista de 10 números reais e mostre-os na ordem inversa.
 """
 
+numeros = []
+
+for num in range(10):
+    numeros.append(input('Informe um número inteiro: '))
+
+print('Imprimindo lista em ordem reversa...')
+numeros.reverse()
+
+print(numeros)
