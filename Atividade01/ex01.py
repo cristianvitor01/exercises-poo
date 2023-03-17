ec3b37ca038a3002e8f98f0606b828a3a78f6fe2
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-17 16:35:40 -0300

    ex01 update

--- Atividade01/ex01.py
+++ Atividade01/ex01.py
@@ -2,9 +2,10 @@
 1-Faça um Programa que receba uma lista de 5 números inteiros e mostre-os.
 """
 
-lista = list()
+lista = []
 
-for i in range(5):
-    lista.append(input('Informe um número: '))
+for item in range(5):
+    lista.append(input('Informe um número inteiro: '))
 
+print('Imprimindo lista...')
 print(lista)
