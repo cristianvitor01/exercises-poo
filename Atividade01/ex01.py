1fa61a784947b41ec183d9a1eecd649aa50db58e
Cristian Vitor <94191697+cristianvitor01@users.noreply.github.com>
2023-03-17 12:35:53 +0000

    add folder Atividade01 and setup

---
+++ Atividade01/ex01.py
@@ -0,0 +1,8 @@
+"""
+1-Faça um Programa que receba uma lista de 5 números inteiros e mostre-os.
+"""
+
+lista = list(input('Informe um número: '))
+
+for num in lista:
+    print(num)
---
+++ Atividade01/ex02.py
@@ -0,0 +1,4 @@
+"""
+2-Faça um Programa que receba uma lista de 10 números reais e mostre-os na ordem inversa.
+"""
+
