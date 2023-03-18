2871f764b95dde77ddd411629e931fe15b19748e
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-18 11:58:15 -0300

    add folder Atividade02 and setup

---
+++ Atividade02/ex01.py
@@ -0,0 +1,4 @@
+"""
+Crie um dicionário que é uma agenda e coloque nele os seguintes dados:
+chave (cpf), nome, idade, telefone. O programa deve ler um número indeterminado de dados, criar a agenda e imprimir todos os itens do dicionário no formato chave: nome-idade-fone.
+"""
---
+++ Atividade02/ex03.py
@@ -0,0 +1,6 @@
+"""
+3-Considere um sistema onde os dados são armazenados em dicionários. Nesse sistema existe um dicionario principal e o dicionário de backup.
+Cada vez que o dicionário principal atinge tamanho 5, ele imprime os dados na tela e apaga o seu conteúdo. 
+Crie um programa que insira dados em um dicionário, realizando o backup a cada dado e excluindo os dados do dicionário principal
+quando ele atingir tamanho 5.
+"""
