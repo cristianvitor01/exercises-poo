ca29d1afe6832f3db19af56dfb663e830711119c
cristianvitor01 <cristianvitoralves@gmail.com>
2023-03-20 17:34:38 -0300

    done ex01

--- Atividade02/ex01.py
+++ Atividade02/ex01.py
@@ -1,4 +1,29 @@
 """
 Crie um dicionário que é uma agenda e coloque nele os seguintes dados:
-chave (cpf), nome, idade, telefone. O programa deve ler um número indeterminado de dados, criar a agenda e imprimir todos os itens do dicionário no formato chave: nome-idade-fone.
+chave (cpf), nome, idade, telefone. O programa deve ler um número indeterminado de dados,
+criar a agenda e imprimir todos os itens do dicionário no formato chave: nome-idade-fone.
 """
+
+agenda = {}
+
+# para o número indeterminado de dados.
+while True:
+    cpf = input('Digite o CPF: ')
+
+    # o laço para quando o cpf não é informado.
+    if not cpf:
+        break
+    
+    nome = input('Digite o nome: ')
+    idade = input('Digite a idade: ')
+    telefone = input('Digite o telefone: ')
+    
+    agenda[cpf] = {
+        'nome': nome,
+        'idade': idade,
+        'telefone': telefone
+    }
+
+print('Agenda:')
+for cpf, dados in agenda.items():
+    print(f"{dados['nome']}-{dados['idade']}-{dados['telefone']}")
