4d0f8cf3dba5e2b5ef4898e1d208f0a9a4b6e85b
Cristian Vitor <94191697+cristianvitor01@users.noreply.github.com>
2023-03-22 11:42:54 +0000

    done ex03.py

--- Atividade02/ex03.py
+++ Atividade02/ex03.py
@@ -4,3 +4,22 @@ Cada vez que o dicionário principal atinge tamanho 5, ele imprime os dados na t
 Crie um programa que insira dados em um dicionário, realizando o backup a cada dado e excluindo os dados do dicionário principal
 quando ele atingir tamanho 5.
 """
+
+principal = {}
+backup = {}
+
+while True:
+    chave = input("Digite a chave para inserir no dicionário: ")
+    valor = input("Digite o valor para a chave: ")
+    principal[chave] = valor
+    backup[chave] = valor
+
+    if len(principal) == 5:
+        print("Dados do dicionário principal:")
+        print(principal)
+        principal = {}
+
+    if len(backup) == 10:
+        print("Realizando backup dos dados:")
+        print(backup)
+        backup = {}
