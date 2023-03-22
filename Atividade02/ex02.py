64044a12d37b481f6eb3d160163982b0f1ab13f2
Cristian Vitor <94191697+cristianvitor01@users.noreply.github.com>
2023-03-22 11:33:05 +0000

    done ex02

--- Atividade02/ex02.py
+++ Atividade02/ex02.py
@@ -2,3 +2,28 @@
 2-Crie um programa que cadastre informações de várias pessoas (nome, idade e cpf) e depois coloque em um dicionário.
 Depois remova todas as pessoas menores de 18 anos do dicionário e coloque em outro dicionário.
 """
+
+pessoas = {}
+menores = {}
+
+while True:
+    cpf = input('Digite o CPF: ')
+
+    # o laço para quando o cpf não é informado.
+    if not cpf:
+        break
+    
+    nome = input('Digite o nome: ')
+    idade = int(input('Digite a idade: '))
+
+    pessoas[cpf] = {
+        'nome': nome,
+        'idade': idade,
+    }
+
+    if idade < 18:
+        menores[nome] = pessoas
+
+
+print("Pessoas menores de 18 anos: ")
+print(menores)
