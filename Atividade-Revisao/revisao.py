b7ab18fd3104c6f011ff1cdb0739b16b405ee82a
cristianvitor01 <cristianvitoralves@gmail.com>
2023-04-10 16:44:17 -0300

    add function addtel

--- Atividade-Revisao/revisao.py
+++ Atividade-Revisao/revisao.py
@@ -3,20 +3,21 @@ funcionarios = [
         "nome": "cristian",
         "cpf": "111111",
         "cargo": "devpleno",
-        "salario": "8000",
+        "salario": 8000,
         "telefones": [159159, 148148, 126126],
     },
     {
         "nome": "func2",
         "cpf": "22222",
         "cargo": "func",
-        "salario": "1000",
+        "salario": 1000,
         "telefones": [11111, 22222, 22222],
     }
 ]
 
 
 def PesquisarFunc(cpf):
+    """Função que pesquisa funcionário pelo seu cpf e exibe na tela seus dados."""
     encontrado = False
     for f in funcionarios:
         if f['cpf'] == cpf:
@@ -28,7 +29,28 @@ def PesquisarFunc(cpf):
         print('Funcionário não encontrado.')
 
 
+def addTel(cpf, telefone):
+    """Encontra funcionário e adiciona um novo telefone."""
+    encontrado = False
+    for f in funcionarios:
+        if f['cpf'] == cpf:
+            #  adicionar um novo telefone
+            f['telefones'].append(telefone)
+            encontrado = True
+            break
+    if encontrado:
+        print('Novo telefone adicionado com sucesso.')
+        print()
+    else:
+        print('Funcionário não encontrado.')
+        print()
 
-cpf = int(input('Informe um CPF: '))
+
+cpf = input('Informe um CPF: ')
 PesquisarFunc(cpf)
+        
+cpf = input('Informe um CPF: ')
+telefone = int(input('Informe um telefone para adicionar: '))
+addTel(cpf, telefone)
 
+print(funcionarios)
