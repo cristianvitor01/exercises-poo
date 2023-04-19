015e002e9fa69540338ceb0e1b40f55aa45a8e7b
Cristian Vitor <94191697+cristianvitor01@users.noreply.github.com>
2023-04-19 13:59:45 +0000

    stand by

--- Avaliação-01/avaliacao.py
+++ Avaliação-01/avaliacao.py
@@ -31,7 +31,6 @@ abastecimentos={1:{"veiculo":veiculos["BCC009"],"valor":400.0,"data":"4-2-2023",
 
 
 def cadastrar_novo_motorista(cod, nome, cpf, rg, cnh):
-    
     motoristas.update({
         cod: {
             "nome": nome,
@@ -40,37 +39,102 @@ def cadastrar_novo_motorista(cod, nome, cpf, rg, cnh):
             "cnh": cnh,
         }
     })
-
     print()
+    print('Motorista cadastrado!')
     print(motoristas)
 
 
-def pesquisar_motorista():
+def pesquisar_motorista(cod):
+    encontrado = False
+    for k, v in motoristas.items():
+        if k == cod:
+            print()
+            print('Motorista encontrado: ')
+            print()
+            print(k, v)
+            encontrado = True
+            break
+    if not encontrado:
+        print('Motorista não encontrado.')
+
+
+def editar_motorista(cod, **kwargs):
     pass
 
-def editar_motorista():
+
+def deletar_motorista(cod):
+    pesquisar_motorista(cod)
+    motoristas.pop(cod)
+
+    print()
+    print('Motorista deletado!')
+    print(motoristas)
+
+def cadastrar_veic(placa, marca, modelo, ano, placa, chassi, cor, km):
+    veiculos.update({
+        placa: {
+            "marca": marca,
+            "modelo": modelo,
+            "ano": ano,
+            "placa": placa,
+            "chassi": chassi,
+            "cor": placa,
+            "km": km,
+        }
+    })
+    print()
+    print('Veículo cadastrado!')
+    print(veiculos)
+
+def pesquisar_veic(placa):
+    encontrado = False
+    for k, v in veiculos.items():
+        if k == placa:
+            print()
+            print('Veículo encontrado: ')
+            print()
+            print(k, v)
+            encontrado = True
+            break
+    if not encontrado:
+        print('Veículo não encontrado.')
+
+def editar_veic():
     pass
 
-def deletar_motorista():
+def deletar_veic():
     pass
 
+def ver_km():
+    pass
 
+def cadastrar_viagem():
+    pass
+
+def editar_viagem():
+    pass
+
+def registrar_abastecimento():
+    pass
+
+def registrar_manut():
+    pass
 
 def menu():
     print("[1] Cadastrar Novo Motorista")
     print("[2] Pesquisar Motorista")
     print("[3] Editar Motorista")
     print('[4] Deletar Motorista')
-    print('[4] Cadastrar Veículo')
-    print('[4] Pesquisar Veículo')
-    print('[4] Editar Veículo')
-    print('[4] Deletar Veículo')
-    print('[4] Ver quilomentragem do veículo')
-    print('[4] Cadastar Viagem')
-    print('[4] Editar Viagem')
-    print('[4] Registrar Abastecimento')
-    print('[4] Registrar Manutenção')
-    print('[4] Relatório')
+    print('[5] Cadastrar Veículo')
+    print('[6] Pesquisar Veículo')
+    print('[7] Editar Veículo')
+    print('[8] Deletar Veículo')
+    print('[10] Ver quilomentragem do veículo')
+    print('[11] Cadastar Viagem')
+    print('[12] Editar Viagem')
+    print('[13] Registrar Abastecimento')
+    print('[14] Registrar Manutenção')
+    print('[15] Relatório')
     print("[0] Sair.")
 
 menu()
@@ -86,12 +150,36 @@ while opc != 0:
         cadastrar_novo_motorista(cod, nome, cpf, rg, cnh)
         break
     elif opc == 2:
-        pass
+        print('Pesquisar Motorista.')
+        cod = input('Informe o CPF/COD: ')
+        pesquisar_motorista(cod)
         break
     elif opc == 3:
-        pass
+        print('Editar Motorista.')
         break
     elif opc == 4:
+        print('Deletar Motorista.')
+        cod = input('Informe o CPF/COD: ')
+        deletar_motorista(cod)
+        break
+    elif opc == 5:
+        print('Cadastrar Veículo')
+        pass
+        break
+    elif opc == 6:
+        print('Pesquisar Veículo')
+        pass
+        break
+    elif opc == 7:
+        print('Editar Veículo')
+        pass
+        break
+    elif opc == 8:
+        print('Deletar Veículo')
+        pass
+        break
+    elif opc == 9:
+        print('Ver quilometragem de Veículo')
         pass
         break
     else:
